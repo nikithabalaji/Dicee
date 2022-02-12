9a3339410b3f4d18c9d848c177ea8d0f27444c17
nikithabalaji <64517438+nikithabalaji@users.noreply.github.com>
2022-02-12 11:40:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNumber1= Math.floor(Math.random()*6)+1;
+var image1= "images/dice"+ randomNumber1+ ".png";
+document.querySelector(".img1").setAttribute("src", image1);
+var randomNumber2= Math.floor(Math.random()*6)+1;
+var image2= "images/dice"+ randomNumber2+ ".png";
+document.querySelector(".img2").setAttribute("src", image2);
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML= "🚩Player 1 wins!";
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML= "Player 2 wins 🚩!";
+}
+else if(randomNumber1===randomNumber2){
+    document.querySelector("h1").innerHTML= "Draw!";
+}
